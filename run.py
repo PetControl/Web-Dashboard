2091ff19ad35aece761d15c96609313ad10d3a04
TristanWiley <tristan@tristanwiley.com>
2016-08-07 02:50:26 -0400

    HYPEHYPEHYPEHYPE

--- run.py
+++ run.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template, request, redirect
 import twilio.twiml
 from PIL import Image
 import urllib2 as urllib
+import cookielib
 import io
 import zbarlight
 
 app = Flask(__name__)
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
 def notifyOwner(petID):
     number = getInfo(petID, "ownerNumber")
@@ -17,7 +24,14 @@ def petExits(petID):
     return True
 
 def parseQRCode(imageURI):
-    fd = urllib.urlopen(get_redirected_url(imageURI))
+    req = urllib2.Request(imageURI, headers=hdr)
+
+    try:
+        page = urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        print e.fp.read()
+
+    fd = urllib.urlopen(page.url)
     image_file = io.BytesIO(fd.read())
     image = Image.open(image_file)
     image.load()
